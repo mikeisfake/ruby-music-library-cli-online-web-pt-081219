76a0855f56ce2f5dedcfd633ee81fc5726639374
Mike Cooper <mikescooper@icloud.com>
2019-09-28 09:44:34 +0000

    Automatically backed up by Learn

--- lib/music_library_cli.rb
+++ lib/music_library_cli.rb
@@ -12,7 +12,7 @@ class MusicLibraryController
   
   def call 
     input = ""
-      while input != "exit"
+    while input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -22,6 +22,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+      
       input = gets.strip
       
       case input
@@ -38,7 +39,7 @@ class MusicLibraryController
       when "play song"
         play_song
       end 
-      end 
+    end 
   end 
   
   def list_songs 
